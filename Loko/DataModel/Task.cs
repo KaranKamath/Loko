c3c440f4320401705546253514f173f0f081400c
Karan Kamath <artemisfowl2.karan@gmail.com>
2014-05-16 02:58:05 +0800

    Add Some Class Framework for DataModel

---
+++ Loko/DataModel/Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace Loko.DataModel
+{
+    public class LokoTask
+    {
+        public string Title { get; set; }
+        public DateTime ExpiryDateTime { get; set; }
+        public ObservableCollection<Geopoint> TaskLocations { get; set; }
+    }
+
+    public class DataSource
+    {
+        private ObservableCollection<LokoTask> _lokoTasks;
+
+        public DataSource()
+        {
+            _lokoTasks = new ObservableCollection<LokoTask>();
+        }
+        
+    }
+}
